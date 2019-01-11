52eed5e657627979f8cf8d41e0c0b100e63c2911
Ashish Prabhu <ashish.pprabhu@gmail.com>
2019-01-11 08:01:03 +0530

    Minor Corrections in programs

--- Methods and Functions/Level 1/04_Old_Mcdonald.py
+++ Methods and Functions/Level 1/04_Old_Mcdonald.py
@@ -9,4 +9,4 @@ def old_mcdonald(anytext):
     if lenText < 4 :
         return ("Please enter Text with more than 4 characters in length")
 
-    return f"{anytext[0].capitalize()}{anytext[1:3]}{anytext[3].capitalize()}{anytext[3:]}"
+    return f"{anytext[0:3].capitalize()}{anytext[3:].capitalize()}"
